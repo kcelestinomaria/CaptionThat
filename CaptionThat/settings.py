aa8ee1ff2d0566944f598cd8fe2e0b09f7cb68e4
unknown <kcelestinomaria127@gmail.com>
2024-12-24 17:03:36 +0300

    huggingface transformers set up

--- CaptionThat/settings.py
+++ CaptionThat/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'transformers',
     'rest_framework', # For API Creation
     # Our App Name
     'ImageCaptionGen',
